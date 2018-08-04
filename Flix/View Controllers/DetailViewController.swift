753bca3a326f8a764e44a66f7ac89b4961e1aa7f
aungphyo88 <aphyo@mail.sfsu.edu>
2018-08-03 18:46:13 -0700

    flix 2

---
+++ Flix/View Controllers/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Flix
+//
+//  Created by AUNG PHYO on 8/3/18.
+//  Copyright © 2018 AUNG PHYO. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var backDropImageView: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: [String: Any]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let movie = movie{
+            titleLabel.text = movie["title"] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
+            
+            let backdropPathString = movie["backdrop_path"] as! String
+            let posterPathString = movie["poster_path"] as! String
+            let baseUrlString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropUrl = URL(string: baseUrlString + backdropPathString)!
+            backDropImageView.af_setImage(withURL: backdropUrl)
+            let posterUrl = URL(string: baseUrlString + posterPathString)!
+            posterImageView.af_setImage(withURL: posterUrl)
+            
+        }
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
--- Flix/View Controllers/NowPlayingViewController.swift
+++ Flix/View Controllers/NowPlayingViewController.swift
@@ -77,6 +77,15 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource,UITableV
         return cell
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
