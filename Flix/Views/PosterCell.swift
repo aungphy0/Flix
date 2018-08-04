a7384b4e1b7878bb33a3f2c19520d11eb14e3a71
aungphyo88 <aphyo@mail.sfsu.edu>
2018-08-03 20:24:43 -0700

    flix 2

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by AUNG PHYO on 8/3/18.
+//  Copyright © 2018 AUNG PHYO. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterPathView: UIImageView!
+}
